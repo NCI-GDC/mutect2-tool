b9b0751b44d188e7cc46be3f1eb05e6f6cdc4762
Shenglai Li <sli6@uchicago.edu>
2016-03-30 16:52:51 -0500

    fix(tool): rm dbsnp and cosmic

--- mutect-tool/tools/mutect_tool.py
+++ mutect-tool/tools/mutect_tool.py
@@ -51,7 +51,7 @@ def fai_chunk(fai_path, blocksize):
         for i in range(1, l, blocksize):
             yield (seq, i, min(i+blocksize-1, l))
 
-def mutect2_cmd_template(gatk_path, ref, fai_path, blocksize, java_heap, cosmic, dbsnp, pon, output_base, contEst, normal_bam_path, tumor_bam_path):
+def mutect2_cmd_template(gatk_path, ref, fai_path, blocksize, java_heap, pon, output_base, contEst, normal_bam_path, tumor_bam_path):
     template = string.Template("/usr/bin/time -v java -Djava.io.tmpdir=/tmp/job_tmp -d64 -jar -Xmx${JAVA_HEAP} -XX:ParallelGCThreads=1 ${GATK_PATH} -T MuTect2 -nct 1 -R ${REF} -L ${REGION} -I:tumor ${TUMOR_BAM} -I:normal ${NORMAL_BAM} --normal_panel ${PON} --contamination_fraction_to_filter ${CONTAMINATION} -o ${OUTPUT_BASE}.${BLOCK_NUM}.mt2.vcf --output_mode EMIT_VARIANTS_ONLY --disable_auto_index_creation_and_locking_when_reading_rods")
     for i, block in enumerate(fai_chunk(fai_path, blocksize)):
         cmd = template.substitute(dict(REF = ref, REGION = '%s:%s-%s' % (block[0], block[1], block[2]), BLOCK_NUM = i),
@@ -60,13 +60,11 @@ def mutect2_cmd_template(gatk_path, ref, fai_path, blocksize, java_heap, cosmic,
                                        NORMAL_BAM = normal_bam_path,
                                        TUMOR_BAM = tumor_bam_path,
                                        CONTAMINATION = contEst,
-                                       COSMIC = cosmic,
-                                       DBSNP = dbsnp,
                                        PON = pon,
                                        OUTPUT_BASE = output_base)
         yield cmd, "%s.%s.mt2.vcf" % (output_base, i)
 
-def run_mutect(case_id, normal_id, normal_bam_path, tumor_id, tumor_bam_path, thread_count, java_heap, reference_fasta_path, contEst, cosmic_path, pon_path, fai_path, blocksize, known_snp_vcf_path, engine, logger):
+def run_mutect(case_id, normal_id, normal_bam_path, tumor_id, tumor_bam_path, thread_count, java_heap, reference_fasta_path, contEst, pon_path, fai_path, blocksize, engine, logger):
   files = [normal_id, tumor_id]
   step_dir = os.path.join(os.getcwd(), 'mutect2')
   os.makedirs(step_dir, exist_ok=True)
@@ -91,8 +89,6 @@ def run_mutect(case_id, normal_id, normal_bam_path, tumor_id, tumor_bam_path, th
                                    normal_bam_path = normal_bam_path,
                                    tumor_bam_path = tumor_bam_path,
                                    contEst = contEst,
-                                   cosmic = cosmic_path,
-                                   dbsnp = known_snp_vcf_path,
                                    pon = pon_path,
                                    output_base = os.path.join(step_dir, 'output'))
     )
