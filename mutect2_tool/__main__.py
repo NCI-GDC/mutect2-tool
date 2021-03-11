76efe80df2f87a84e7cd2c5296bb643f9cc27a31
Charles Czysz <czysz@uchicago.edu>
2021-03-11 08:33:38 -0600

    Remove extra code

--- mutect2_tool/__main__.py
+++ mutect2_tool/__main__.py
@@ -3,16 +3,6 @@
 from mutect2_tool import multi_mutect2
 
 if __name__ == "__main__":
-    # CLI Entrypoint.
-    retcode = 0
-
-    try:
-        retcode = multi_mutect2.main()
-    except Exception as e:
-        retcode = 1
-        logger.exception(e)
-
-    sys.exit(retcode)
-
+    multi_mutect2.main()
 
 # __END__
