cb7ba1808693deba348e3b917789f71af05c29bb
Shenglai Li <lyellooo@gmail.com>
2021-03-15 16:38:01 -0500

    BINF-481: update entry (#16)
    * BINF-481: update entry
    * BINF-481: better debug

--- mutect2_tool/__main__.py
+++ mutect2_tool/__main__.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+import sys
 
 from mutect2_tool import multi_mutect2
 
 if __name__ == "__main__":
-    multi_mutect2.main()
+    # CLI Entrypoint.
+    retcode = 0
+
+    try:
+        retcode = multi_mutect2.main()
+    except Exception as e:
+        retcode = 1
+
+    sys.exit(retcode)
 
 # __END__
